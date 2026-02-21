68370f170894aa01e644cad8645a555ec45746e3
Wendell Richards <wenrich85@gmail.com>
2026-02-21 11:45:37 -0600

    Initial Commit

---
+++ MDI_113_CH4_class_work Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDI_113_CH4_class_work Watch App
+//
+//  Created by Wendell Richards on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDI_113_CH4_class_work Watch App/MDI_113_CH4_class_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDI_113_CH4_class_workApp.swift
+//  MDI_113_CH4_class_work Watch App
+//
+//  Created by Wendell Richards on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MDI_113_CH4_class_work_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
